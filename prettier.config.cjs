b206c819bdf4046505740e32d587336bfdb5c78a
Dustin Griffith <dustingriffith@outlook.com>
2023-04-06 23:45:24 -0400

    remove rome

---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+    endOfLine: 'lf',
+    htmlWhitespaceSensitivity: 'ignore',
+    printWidth: 100,
+    proseWrap: 'preserve',
+    quoteProps: 'as-needed',
+    semi: true,
+    singleAttributesPerLine: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: false,
+  },
+];
