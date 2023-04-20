4710fcea6850fa07ecc6052e1cd227136e00af28
Dustin Griffith <djarekg@users.noreply.github.com>
2023-04-20 00:34:10 -0400

    site menu avatar

--- apps/demo/src/components/settings-menu/settings-menu.ts
+++ apps/demo/src/components/settings-menu/settings-menu.ts
@@ -1,16 +1,18 @@
 import { RouteTypes } from '#/router/route-types.js';
+import { Task } from '@lit-labs/task';
 import '@material/web/button/text-button.js';
 import '@material/web/switch/switch.js';
 import type { MdSwitch } from '@material/web/switch/switch.js';
 import { useInject } from '@rask/core/di/inject.js';
 import type { TypeEvent } from '@rask/core/events/type-event.js';
 import { AuthService } from '@rask/identity/services/auth-service.js';
+import { UserService } from '@rask/identity/services/user-service.js';
 import '@rask/web/menu/menu.js';
 import type { Menu } from '@rask/web/menu/menu.js';
 import { useTheme } from '@rask/web/theme/index.js';
 import { Router } from '@vaadin/router';
 import { LitElement, html, type TemplateResult } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 import { createRef, ref, type Ref } from 'lit/directives/ref.js';
 import css from './settings-menu.css' assert { type: 'css' };
 
@@ -20,15 +22,25 @@ const theme = useTheme();
 export class SettingsMenu extends LitElement {
   static override styles = [css];
 
+  #getInitials = new Task(
+    this,
+    async () => await this.#loadUser(),
+    () => [null]
+  );
+
   #authService = useInject(AuthService);
+  #userService = useInject(UserService);
   #menu: Ref<Menu> = createRef();
 
   @property({ attribute: false }) sittingsMenu: Ref<Menu> = createRef();
   @property({ type: Boolean }) themeDarkModeSelected = theme.isDark;
 
+  @state() private initials = '';
+
   override render(): TemplateResult {
     return html`
       <rk-menu ${ref(this.#menu)} icon="account_circle" .open>
+        ${this.#renderMenuTrigger()}
         <label>
           Dark Mode
           <md-switch .selected="${this.themeDarkModeSelected}" @change="${this.#toggleTheme}"
@@ -47,6 +59,19 @@ export class SettingsMenu extends LitElement {
         </div>
       </rk-menu>
     `;
+  }
+
+  #renderMenuTrigger(): TemplateResult {
+    return html`${this.#getInitials.render({
+      complete: () => html` <span slot="trigger" class="avatar">${this.initials}</span> `,
+    })}`;
+  }
+
+  async #loadUser(): Promise<void> {
+    const user = await this.#userService.getCurrentUser();
+    const { firstName, lastName } = user;
+
+    this.initials = `${firstName[0]}${lastName[0]}`;
   }
 
   #toggleTheme({ target }: TypeEvent<MdSwitch>): void {
